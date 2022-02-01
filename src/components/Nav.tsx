21e40b3104893e8dfa6698e6c35fc22c23b7efc2
wuzx9905 <853529718@qq.com>
2022-02-01 20:56:18 +0800

    搞定三个 icons

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,6 +1,9 @@
 import styled from 'styled-components';
 import {Link} from 'react-router-dom';
 import React from 'react';
+require( 'icons/tag.svg');  //svgo-loader; svg-sprite-loader(声称标签)
+require('icons/money.svg');
+require( 'icons/chart.svg')
 
 const NavWrapper = styled.nav`
   box-shadow: 0 0 3px rgba(0,0,0,0.25);
@@ -10,7 +13,14 @@ const NavWrapper = styled.nav`
     >li{
       width: 33.333%;
       text-align: center;
-      padding: 18px;
+      display:flex;
+      flex-direction: column;
+      align-items: center;
+      padding: 4px 0;
+      .icon{
+        width: 24px;
+        height: 24px;
+      }
     }
   }
 `
@@ -20,12 +30,21 @@ const Nav = ()=>{
         <NavWrapper>
             <ul>
                 <li>
+                    <svg className="icon">
+                        <use xlinkHref="#tag"/>
+                    </svg>
                     <Link to="/tags">标签页</Link>
                 </li>
                 <li>
+                    <svg className="icon">
+                        <use xlinkHref="#money"/>
+                    </svg>
                     <Link to="/money">记账页</Link>
                 </li>
                 <li>
+                    <svg className="icon">
+                        <use xlinkHref="#chart"/>
+                    </svg>
                     <Link to="/statistics">统计页</Link>
                 </li>
             </ul>
