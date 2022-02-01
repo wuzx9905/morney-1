4429352c570255e7a077434a7765dbe076528e9c
wuzx9905 <853529718@qq.com>
2022-02-01 23:39:47 +0800

    Money.tsx 之 备注 UI 完成

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -5,6 +5,7 @@ import Icon from './Icon';
 
 
 const NavWrapper = styled.nav`
+  background: #ffffff;
   box-shadow: 0 0 3px rgba(0,0,0,0.25);
   line-height: 20px;
   >ul{
--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -27,7 +27,24 @@ const TagsSection = styled.section`
 `;
 
 const NotesSection = styled.section`
-
+  background: #f1f4f4;
+  padding: 0 16px;
+  font-size: 14px;
+  >label{
+    display:flex;
+    align-items: center;
+    >span{
+      margin-right: 16px;
+      white-space: nowrap;
+    }
+    >input{
+      display:block;
+      width: 100%;
+      height: 70px;
+      background: none;
+      border:none;
+    }
+  }
 `;
 const CategorySection = styled.section`
 
@@ -52,7 +69,7 @@ function Money() {
             <NotesSection>
                 <label>
                     <span>备注</span>
-                    <input type="text"/>
+                    <input type="text" placeholder="在这里添加备注"/>
                 </label>
             </NotesSection>
             <CategorySection>
