18b0174f9990fbcfb2243f47a19aacdc0a83be1e
wuzx9905 <853529718@qq.com>
2022-02-01 21:33:27 +0800

    创建 Layout 组件

--- src/App.tsx
+++ src/App.tsx
@@ -4,45 +4,29 @@ import {
     Route,
     HashRouter as Router, Switch, Redirect
 } from 'react-router-dom';
-import styled from 'styled-components';
-import Nav from './components/Nav';
+import Nav from 'components/Nav';
+import Layout from 'components/Layout';
 
-const Wrapper = styled.div`
-  height: 100vh;
-  display: flex;
-  flex-direction: column;
-`;
-
-const Main = styled.div`
-  flex-grow: 1;
-  overflow: auto;
-`;
 
 function App() {
     return (
         <Router>
-            <Wrapper>
-                <Main>
-                    <Switch>
-                        <Route path="/tags">
-                            <Tags/>
-                        </Route>
-                        <Route path="/money">
-                            <Money/>
-                        </Route>
-                        <Route path="/statistics">
-                            <Statistics/>
-                        </Route>
-                        <Redirect exact from={'/'} to={'/money'}/>
-                        <Route path="*">
-                            <NoMatch/>
-                        </Route>
-                    </Switch>
-                </Main>
+            <Switch>
+                <Route path="/tags">
+                    <Tags/>
+                </Route>
+                <Route path="/money">
+                    <Money/>
+                </Route>
+                <Route path="/statistics">
+                    <Statistics/>
+                </Route>
+                <Redirect exact from={'/'} to={'/money'}/>
+                <Route path="*">
+                    <NoMatch/>
+                </Route>
+            </Switch>
 
-                <Nav/>
-
-            </Wrapper>
         </Router>
     );
 }
@@ -56,15 +40,21 @@ function NoMatch() {
 }
 
 function Tags() {
-    return <h2>标签页面</h2>;
+    return (
+        <Layout><h2>标签页面</h2></Layout>
+    );
 }
 
 function Money() {
-    return <h2>记账页面</h2>;
+    return (
+        <Layout><h2>记账页面</h2></Layout>
+    );
 }
 
 function Statistics() {
-    return <h2>统计页面</h2>;
+    return (
+        <Layout><h2>统计页面</h2></Layout>
+    );
 }
 
 export default App;
---
+++ src/components/Icon.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+// require( 'icons/tag.svg');  //svgo-loader; svg-sprite-loader(生成标签)
+// require('icons/money.svg');
+// require( 'icons/chart.svg')
+//require 一个目录
+let importAll = (requireContext: __WebpackModuleApi.RequireContext)=>requireContext.keys().forEach(requireContext);
+try {
+    importAll(require.context('icons',true,/\.svg$/));
+}
+catch (error){
+    console.log(error)
+}
+
+type Props = {
+    name: string
+}
+
+const Icon = (props:Props)=>{
+    return(
+        <svg className="icon">
+            <use xlinkHref={'#'+props.name}/>
+        </svg>
+    )
+}
+
+export default Icon;
