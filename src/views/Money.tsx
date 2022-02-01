ffb43ff9680db58d68b0b8af11a751c63deb1d32
wuzx9905 <853529718@qq.com>
2022-02-02 00:23:37 +0800

    Money.tsx 之 UI 完成

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -5,6 +5,11 @@ import styled from 'styled-components';
 const TagsSection = styled.section`
   background: #FFFFFF;
   padding: 12px 16px;
+  flex-grow: 1;
+  display:flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: flex-start;
   > ol {
     margin: 0 -12px;
     > li {
@@ -129,10 +134,14 @@ const NumberPadSection = styled.section`
    } 
 `;
 
+const MyLayout = styled(Layout)`
+  display:flex;
+  flex-direction: column;
+`
 
 function Money() {
     return (
-        <Layout>
+        <MyLayout>
             <TagsSection>
                 <ol>
                     <li>衣</li>
@@ -175,7 +184,7 @@ function Money() {
                     <button>.</button>
                 </div>
             </NumberPadSection>
-        </Layout>
+        </MyLayout>
     );
 }
 
