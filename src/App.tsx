053100fef79270a36647f74c3177b6f25f96d39d
wuzx9905 <853529718@qq.com>
2022-02-01 20:22:11 +0800

    添加 svg 文件

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,70 @@
 import React from 'react';
+import {
+    Link,
+    Route,
+    HashRouter as Router, Switch, Redirect
+} from 'react-router-dom';
+import styled from 'styled-components';
+import Nav from './components/Nav';
 
+const Wrapper = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Main = styled.div`
+  flex-grow: 1;
+  overflow: auto;
+`;
 
 function App() {
     return (
-        <div>
-            diu
-        </div>
+        <Router>
+            <Wrapper>
+                <Main>
+                    <Switch>
+                        <Route path="/tags">
+                            <Tags/>
+                        </Route>
+                        <Route path="/money">
+                            <Money/>
+                        </Route>
+                        <Route path="/statistics">
+                            <Statistics/>
+                        </Route>
+                        <Redirect exact from={'/'} to={'/money'}/>
+                        <Route path="*">
+                            <NoMatch/>
+                        </Route>
+                    </Switch>
+                </Main>
+
+                <Nav/>
+
+            </Wrapper>
+        </Router>
     );
+}
+
+function NoMatch() {
+    return (
+        <h2>
+            地址错误，页面不存在
+        </h2>
+    );
+}
+
+function Tags() {
+    return <h2>标签页面</h2>;
+}
+
+function Money() {
+    return <h2>记账页面</h2>;
+}
+
+function Statistics() {
+    return <h2>统计页面</h2>;
 }
 
 export default App;
