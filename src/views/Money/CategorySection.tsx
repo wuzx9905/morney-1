8476fe0e8eaf7c9764abe5ff51e024f2cce9dfaf
wuzx9905 <853529718@qq.com>
2022-02-02 21:59:43 +0800

    numberPad 功能 wip(working in progress)

--- src/views/Money/CategorySection.tsx
+++ src/views/Money/CategorySection.tsx
@@ -33,7 +33,9 @@ const CategorySection:React.FunctionComponent  = ()=>{
         <Wrapper>
         <ul>
             {categoryList.map(c =>
-                <li className={category ===c?'selected':''}
+                <li
+                    key={c}
+                    className={category ===c?'selected':''}
                                       onClick={()=>{setCategory(c)}}
             >{categoryMap[c]}</li>)}
         </ul>
