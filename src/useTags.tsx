e56d50c6751ebd06bed88a951d5e516430ae3c46
wuzx9905 <853529718@qq.com>
2022-02-04 22:29:53 +0800

    删除 tag

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -30,7 +30,17 @@ const useTags = () => { //封装自定义 Hook
         tagsClone.splice(index, 1, {id: id, name: obj.name});
         setTags(tagsClone); //不可变数据，不能直接修改原变量
     };
-    return {tags, setTags, findTag, updateTag, findTagIndex};
+    const deleteTag = (id:number)=>{
+        //获取下标
+        const index = findTagIndex(id);
+        //深拷贝tags
+        const tagsClone = JSON.parse(JSON.stringify(tags));
+        //把tagsClone的第index个删掉，换成{}
+        tagsClone.splice(index, 1);
+        setTags(tagsClone)
+    }
+    return {tags, setTags, findTag, updateTag, findTagIndex,deleteTag};
+
 };
 
 export {useTags};
--- src/views/Tag.tsx
+++ src/views/Tag.tsx
@@ -14,43 +14,58 @@ type Params = {
 }
 
 const Topbar = styled.header`
-  display:flex;
+  display: flex;
   justify-content: space-between;
   align-items: center;
   line-height: 20px;
   padding: 14px;
   background: white;
-`
+`;
 
 const InputWrapper = styled.div`
   background: white;
   padding: 0 16px;
   margin-top: 8px;
-`
+`;
 
 const Tag: React.FC = () => {
-    const {findTag,updateTag} = useTags();
-    let {id:idString} = useParams<Params>();
+    const {findTag, updateTag, deleteTag} = useTags();
+    let {id: idString} = useParams<Params>();
     const tag = findTag(parseInt(idString));
-    return (
-        <Layout>
-            <Topbar>
-                <Icon name="left"/>
-                <span>编辑标签</span>
-                <Icon/>
-            </Topbar>
+    const tagContent =(tag:{id:number;name:string})=> (
+        <div>
             <InputWrapper>
                 <Input label="标签名" type="text" placeholder="标签名"
-                value={tag.name}
-                       onChange={(e)=>{updateTag(tag.id,{name:e.target.value})}}
+                       value={tag.name}
+                       onChange={(e) => {updateTag(tag.id, {name: e.target.value});}}
                 />
             </InputWrapper>
             <Center>
                 <Space/>
                 <Space/>
                 <Space/>
-                <Button>删除标签</Button>
+                <Button onClick={() => {deleteTag(tag.id);}}>删除标签</Button>
             </Center>
+        </div>
+    )
+
+
+    return (
+
+        <Layout>
+            <Topbar>
+                <Icon name="left"/>
+                <span>编辑标签</span>
+                <Icon/>
+            </Topbar>
+
+
+            {tag ? tagContent(tag) :
+                <Center>
+                    <Space/>
+                    tag 不存在
+                </Center>}
+
         </Layout>
     );
 };
