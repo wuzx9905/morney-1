94158623fc479092bfa442f5dae31bedda8a4b2a
wuzx9905 <853529718@qq.com>
2022-02-04 17:56:02 +0800

    封装 fingTag 到 useTags 里

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
         <AppWrapper>
             <Router>
                 <Switch>
-                    <Route exact path="/tags/:tag">
+                    <Route exact path="/tags/:id">
                         <Tag/>
                     </Route>
                     <Route exact path="/tags">
