28643a5741d9868c9ef379530e20149582c1e312
wuzx9905 <853529718@qq.com>
2022-02-04 22:18:12 +0800

    编辑 tag 名完成

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -7,11 +7,30 @@ const defaultTags = [
     {id: createId(), name: '食'},
     {id: createId(), name: '住'},
     {id: createId(), name: '行'}
-]
+];
 const useTags = () => { //封装自定义 Hook
-    const [tags, setTags] = useState<{ id:number; name: string }[]>(defaultTags);
-    const findTag = (id:number)=>tags.filter(tag =>tag.id===id)[0];
-    return {tags, setTags,findTag};
+    const [tags, setTags] = useState<{ id: number; name: string }[]>(defaultTags);
+    const findTag = (id: number) => tags.filter(tag => tag.id === id)[0];
+    const findTagIndex = (id: number) => {
+        let result = -1;
+        for (let i = 0; i < tags.length; i++) {
+            if (tags[i].id === id) {
+                result = i;
+                break;
+            }
+        }
+        return result;
+    };
+    const updateTag = (id: number, obj: { name: string }) => {
+        //获取下标
+        const index = findTagIndex(id);
+        //深拷贝tags
+        const tagsClone = JSON.parse(JSON.stringify(tags));
+        //把tagsClone的第index个删掉，换成{}
+        tagsClone.splice(index, 1, {id: id, name: obj.name});
+        setTags(tagsClone); //不可变数据，不能直接修改原变量
+    };
+    return {tags, setTags, findTag, updateTag, findTagIndex};
 };
 
 export {useTags};
