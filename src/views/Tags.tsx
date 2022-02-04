3878f4ae7bdd024115584afd2b296c0b22d9698c
wuzx9905 <853529718@qq.com>
2022-02-04 17:00:37 +0800

    创建 /tags/:tag 路由，学会 exact 用法

--- src/views/Tags.tsx
+++ src/views/Tags.tsx
@@ -3,24 +3,30 @@ import React from 'react';
 import {useTags} from '../useTags';
 import styled from 'styled-components';
 import Icon from '../components/Icon';
+import { Link } from 'react-router-dom';
 
 const TagList = styled.ol`
   font-size: 16px;
   background: white;
-  >li{
+
+  > li {
     border-bottom: 1px solid #d5d5d9;
     line-height: 24px;
-    padding: 14px 0 14px 0;
     margin-left: 16px;
     margin-right: 16px;
-    display:flex;
-    justify-content: space-between;
-    align-items: center;
-    :last-child{
+    
+    > a{
+      display: flex;
+      padding: 14px 0 14px 0;
+      justify-content: space-between;
+      align-items: center;
+    }
+
+    :last-child {
       border: none;
     }
   }
-`
+`;
 const Button = styled.button`
   font-size: 18px;
   border: none;
@@ -28,30 +34,32 @@ const Button = styled.button`
   border-radius: 4px;
   background: #767676;
   color: white;
-`
+`;
 
 const Center = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
-`
+`;
 
 const Space = styled.div`
   height: 16px;
-`
-
+`;
 
 
 function Tags() {
-    const {tags,setTags} = useTags()//useState
+    const {tags, setTags} = useTags();//useState
     return (
         <Layout>
             <TagList>
-            {tags.map(tag=><li key = {tag}>
-                <span className="oneLine">{tag}</span>
-                <Icon name="right"/>
-            </li>)}
+                {tags.map(tag =>
+                    <li key={tag}>
+                        <Link to={'/tags/'+tag}>
+                        <span className="oneLine">{tag}</span>
+                        <Icon name="right"/>
+                        </Link>
+                    </li>)}
             </TagList>
             <Center>
                 <Space/>
